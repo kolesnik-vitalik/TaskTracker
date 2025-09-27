b38564afaac448459b9aab0eb3126d3a43e6d61d
Kolesnik <kolesnik.vitalik21@gmail.com>
2025-09-27 19:50:26 +0300

    New architectural project, renamed some classes

--- src/main/java/org/example/tasktracker/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.tasktracker.repository;
-
-import org.example.tasktracker.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
