a68db901ed53644b8e6b31d952d21f8cd3cf1f61
Kolesnik <kolesnik.vitalik21@gmail.com>
2025-10-27 15:51:51 +0300

    added project controller swagger documentation

--- src/main/java/org/example/tasktracker/api/controllers/ProjectController.java
+++ src/main/java/org/example/tasktracker/api/controllers/ProjectController.java
@@ -1,5 +1,6 @@
 package org.example.tasktracker.api.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.tasktracker.api.dto.ProjectDto;
@@ -22,6 +23,7 @@ public class ProjectController {
     private final ProjectService projectService;
     private final ProjectDtoConverter projectDtoConverter;
 
+    @Tag(name = "Получение всех проектов", description = "Получение списка всех проектов с опциональным параметром по префиксу")
     @GetMapping
     @Transactional
     public List<ProjectDto> getProjects(
@@ -34,12 +36,14 @@ public class ProjectController {
                 .collect(Collectors.toList());
     }
 
+    @Tag(name = "Удаление проекта", description = "Удаление проекта по id")
     @DeleteMapping("{id}")
     public ResponseEntity<?> deleteById(@PathVariable("id") long projectId) {
         projectService.deleteById(projectId);
         return ResponseEntity.notFound().build();
     }
 
+    @Tag(name = "Создание проекта", description = "Создание проекта по параметру имени")
     @PostMapping
     public ProjectDto createProject(@RequestParam String name) {
 
@@ -49,6 +53,7 @@ public class ProjectController {
 
     }
 
+    @Tag(name = "Редактирование проекта", description = "Изменение названия проекта по его id")
     @PatchMapping("{id}")
     public ProjectDto edit(@PathVariable("id") long projectId,
                                   @RequestParam String name) {
